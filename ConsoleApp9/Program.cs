3f12d4c3633123c8d95954863d1d0bfda457c23c
abdullahehab254037 <abdullahehab173@gmail.com>
2026-09-15 17:24:12 +0300

    question2

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -5,10 +5,16 @@
         static void Main(string[] args)
         {
             #region question1 
-           //Book book1=new Book();
-           //book1.genre = Genre.Science;
-           //Console.WriteLine(book1.genre);
+            //Book book1=new Book();
+            //book1.genre = Genre.Science;
+            //Console.WriteLine(book1.genre);
 
+            #endregion
+
+            #region question2
+            //Console.WriteLine((int)Genre.Fiction);    
+            //Console.WriteLine((int)Genre.NonFiction); 
+            //Console.WriteLine((int)Genre.Science);
             #endregion
         }
     }
