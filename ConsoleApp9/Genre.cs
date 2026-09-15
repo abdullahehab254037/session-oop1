2ff4810f26e81415337fb4a2c35d3c52c2f24edc
abdullahehab254037 <abdullahehab173@gmail.com>
2026-09-15 17:05:36 +0300

    Add project files.

---
+++ ConsoleApp9/Genre.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    public enum Genre
+    {
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+   
+}
