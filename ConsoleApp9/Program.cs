bf17c89980ad60987d9679b210d879b8767127c9
abdullahehab254037 <abdullahehab173@gmail.com>
2026-09-15 17:19:36 +0300

    Book class

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            #region question1 
+            Book book1=new Book();
             
+            book1.genre = Genre.Science;
+
+            #endregion
         }
     }
    
---
+++ ConsoleApp9/book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+    public class Book
+    {
+        public Genre genre;
+    }
+}
